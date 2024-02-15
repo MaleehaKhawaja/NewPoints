2fa28196ec50f58aadc67bed302593ede5adbd3b
MaleehaKhawaja <65968445+MaleehaKhawaja@users.noreply.github.com>
2024-02-15 10:37:03 +0000

    Update X034.m

--- X034.m
+++ X034.m
@@ -1,59 +1,49 @@
 load 'models_and_maps.m';
 load 'rank_0_auxiliary.m';
 
-print "We compute a model for X0(64) using the code given in the paper by
+print "We compute a model for X0(34) using the code given in the paper by
 //Adzaga, Keller, Michaud-Jacobs, Najman, Ozman, and Vukorepa";
 //source: https://github.com/TimoKellerMath/QuadraticPoints
 
-time X64, ws, pairs, NB, cusp := eqs_quos(64,[]);
+time X34, ws, pairs, NB, cusp := eqs_quos(34,[]);
 
-time j, num_denom := jmap(X64, 64);
+time j, num_denom := jmap(X34, 34);
 
-//We need the cusps of X64 to compute the torsion subgroup of J64(Q)
+//We need the cusps of X34 to compute the torsion subgroup of J34(Q)
 
-cusps := compute_cusps(X64, 64, ws, cusp, num_denom: use_jmap := false);
+cusps := compute_cusps(X34, 34, ws, cusp, num_denom: use_jmap := false);
 
-P1 := cusps[1];
-P2 := cusps[2];
-P3 := cusps[3];
-P4 := cusps[4];
-P5 := cusps[5];
-P6 := cusps[6];
-P7 := cusps[7];
+findGenerators(X34,34,cusps,cusps[1],5);
 
-findGenerators(X64,64,cusps,P2,3);
-//We know J(Q)=Z/2Z x Z/4Z x Z/4Z by Ozman and Siksek
+//We know J(Q)=Z/4Z x Z/12Z by Ozman and Siksek
 
-D1 := Place(X64![-1,-1,1])-Place(X64![1,-1,1]);
-D2 := Place(X64![1,1,0])-Place(X64![1,-1,1]);
-D3 := Place(X64![-1,1,0])-Place(X64![1,-1,1]);
-infdiv := Place(X64![1,-1,1]);
+D1:=Place(X34![-1,1,1])-Place(X34![1,-1,1]);
+D2:=Place(X34![1,1,1])-Place(X34![1,-1,1]);
 
-for a in [0..1]
-    do for b in [0..3]
-        do for c in [0..3]
-            do D := 3*infdiv + a*D1 + b*D2 + c*D3;
-            L, phi := RiemannRochSpace(D);
-            dim := Dimension(L);
-            assert dim le 2; 
-        end for;
+infdiv:=Place(X34![1,-1,1]);
+
+for a in [0..3]
+    do for b in [0..11]
+        do D := 3*infdiv + a*D1 + b*D2;
+        L, phi := RiemannRochSpace(D);
+        dim := Dimension(L);
+        assert dim le 2;
     end for;
 end for;
 
 Qu<u>:=FunctionField(Rationals());
 Qux<w,x,y>:=ProjectiveSpace((Qu), 2);
 
-X64 := Curve(Qux, w^4 + 6*w^2*x^2 - 7*x^4 + 24*w^2*x*y + 8*x^3*y + 24*w^2*y^2 + 24*x^2*y^2 + 32*x*y^3 + 16*y^4);
+X34:=Curve(Qux,w^4 + 6*w^2*x^2 + 8*w^2*y^2 - 8*x^4 - 6*x^2*y^2 - y^4);
 
-D1 := Place(X64![-1,-1,1])-Place(X64![1,-1,1]);
-D2 := Place(X64![1,1,0])-Place(X64![1,-1,1]);
-D3 := Place(X64![-1,1,0])-Place(X64![1,-1,1]);
-infdiv := Place(X64![1,-1,1]);
+D1:=Place(X34![-1,1,1])-Place(X34![1,-1,1]);
+D2:=Place(X34![1,1,1])-Place(X34![1,-1,1]);
 
-for a in [0..1] do	
-    for b in [0..3] do
-        for c in [0..3] do
-            D:=a*D1+b*D2+c*D3+3*infdiv;
+infdiv:=Place(X34![1,-1,1]);
+
+for a in [0..3] do	
+    for b in [-6..5] do
+            D:=a*D1+b*D2+3*infdiv;
             L,phi:=RiemannRochSpace(D);
             if Dimension(L) eq 2 then
                 if #Decomposition(D+Divisor(phi(u*L.1+L.2))) eq 1 then
@@ -89,17 +79,12 @@ for a in [0..1] do
                             pts:=[];
                         end if;
                     else
-                        H:=SimplifiedModel(MinimalWeierstrassModel(HyperellipticCurve(g)));
+                        H:=HyperellipticCurve(g);
                         pts:=Points(H : Bound:=10);
-                        print H;
                     end if;
-                    print a, b, c, Genus(H),pts;
-                    print "++++++++++++++++++++";
-                    assert IsLocallySoluble(H, 3) eq false;
-                    print "There are no Q3-points on the hyperelliptic curve, and thus no rational points."; 
+                    print a, b, Genus(H),pts;
                     print "++++++++++++++++++++";
                 end if;
             end if;
-        end for;
     end for;
 end for;
